4654d8c502eae00ea9c5a2b029fa2637abe968a5
kururu <nomemeber@gmail.com>
2022-02-14 22:32:50 +0900

    Init 예시용 snippet 추가

---
+++ shell/pet.sh
@@ -0,0 +1,30 @@
+[[snippets]]
+  description = "git pull"
+  command = "git pull"
+  tag = ["git"]
+  output = ""
+
+[[snippets]]
+  description = "git add all"
+  command = "git add *"
+  tag = ["git"]
+  output = ""
+
+[[snippets]]
+  description = "git add all with dot"
+  command = "git add ./"
+  tag = ["git"]
+  output = ""
+
+[[snippets]]
+  description = "git commit"
+  command = "git commit"
+  tag = ["git"]
+  output = ""
+
+[[snippets]]
+  description = "main branch에서 pull 해라"
+  command = "git pull orgin main"
+  tag = ["git"]
+  output = ""
+
